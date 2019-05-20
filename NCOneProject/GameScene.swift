bfcff3a17a08c5cf9e09e0520bd3102b6f22c49b
Finley Khouwira <fineykhouwira@gmail.com>
2019-05-20 19:56:29 +0700

    Big Bubble added

--- NCOneProject/GameScene.swift
+++ NCOneProject/GameScene.swift
@@ -11,45 +11,71 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    var circle = SKShapeNode()
+    var smallCircle = SKShapeNode()
     
-    var circleOne = SKShapeNode(circleOfRadius: 50)
-    var circleTwo = SKShapeNode(circleOfRadius: 50)
+    var blueCircle = SKShapeNode(circleOfRadius: 60)
+    var yellowCircle = SKShapeNode(circleOfRadius: 60)
+    var redCircle = SKShapeNode(circleOfRadius: 60)
     
-    let moveAction = SKAction.moveBy(x: 10, y: 10, duration: 1)
-    
-    let sprite = SKSpriteNode(color: .gray, size: CGSize(width: 100, height: 100))
+    let colorCollection: [UIColor] = [#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1), #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)]
     
     override func didMove(to view: SKView) {
         
-        circleOne.fillColor = .white
-        circleOne.position = self.view!.center
-        addChild(circleOne)
+        blueCircle.fillColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        blueCircle.position = self.view!.center
+        addChild(blueCircle)
         
-        circleTwo.fillColor = .blue
-        circleTwo.position = CGPoint(x: 414, y: 448)
-        addChild(circleTwo)
+        yellowCircle.fillColor = #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)
+        yellowCircle.position = CGPoint(x: 414, y: 400)
+        addChild(yellowCircle)
         
-        self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
+        redCircle.fillColor = #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)
+        redCircle.position = CGPoint(x: 200, y: 400)
+        addChild(redCircle)
         
-        circleOne.physicsBody = SKPhysicsBody(circleOfRadius: 50)
-        circleOne.physicsBody?.affectedByGravity = true
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: CGRect(x: 0, y: 0, width: self.frame.width, height: 2000))
         
-        circleTwo.physicsBody = SKPhysicsBody(circleOfRadius: 50)
-        circleTwo.physicsBody?.affectedByGravity = true
+        blueCircle.physicsBody = SKPhysicsBody(circleOfRadius: 60)
+        blueCircle.physicsBody?.affectedByGravity = true
+        
+        yellowCircle.physicsBody = SKPhysicsBody(circleOfRadius: 60)
+        yellowCircle.physicsBody?.affectedByGravity = true
+        
+        redCircle.physicsBody = SKPhysicsBody(circleOfRadius: 60)
+        redCircle.physicsBody?.affectedByGravity = true
+        
+        for i in 0...20{
+            let smallCircle = SKShapeNode(circleOfRadius: 60)
+            smallCircle.position = CGPoint(x: CGFloat.random(in: 100...300), y: CGFloat.random(in: 1000...1500))
+            smallCircle.fillColor = colorCollection[Int.random(in: 0...2)]
+            smallCircle.physicsBody = SKPhysicsBody(circleOfRadius: 60)
+            smallCircle.physicsBody?.affectedByGravity = true
+
+            addChild(smallCircle)
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             let location = touch.location(in: self)
             
-            let smallCircle = SKShapeNode(circleOfRadius: 50)
-            smallCircle.position = location
-            smallCircle.fillColor = UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1)
-            smallCircle.physicsBody = SKPhysicsBody(circleOfRadius: 50)
-            smallCircle.physicsBody?.affectedByGravity = true
-            
-            self.addChild(smallCircle)
+            let anotherSmallCircle = SKShapeNode(circleOfRadius: 60)
+            anotherSmallCircle.position = location
+            anotherSmallCircle.fillColor = colorCollection[Int.random(in: 0...2)]
+            anotherSmallCircle.physicsBody = SKPhysicsBody(circleOfRadius: 60)
+            anotherSmallCircle.physicsBody?.affectedByGravity = true
+            self.addChild(anotherSmallCircle)
+        }
+        
+        let touchedYellow = self.atPoint(yellowCircle.position)
+        
+        if let name = touchedYellow.name {
+            if name == "playLbl"
+            {
+                physicsWorld.gravity = CGVector(dx: 0, dy: 3)
+                print("playLbl Touched")
+            }
+        }
         }
     }
-}
+
